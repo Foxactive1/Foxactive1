30f6b1d30ad3215e23d81e894d6ebe406080c3ba
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2023-10-22 15:59:39 -0300

    Add files via upload

---
+++ pipeline python.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        módulo1
+# Purpose:
+#
+# Author:      Victor Hugo
+#
+# Created:     22/10/2023
+# Copyright:   (c) Dione Castro Alves 2023
+# Licence:     InNovaIdeia2023
+#-------------------------------------------------------------------------------
+
+import sqlite3
+import pandas as pd
+
+# Extrai dados do arquivo CSV
+data = pd.read_csv('C:\Dcoder\dio_me_projetos\projeto_dio.csv')
+
+# Cria o banco de dados SQLite ' Dados.db'
+conn = sqlite3.connect('C:\Dcoder\dio_me_projetos\dados.db')
+
+# Passo 3: Carregar os dados no banco de dados
+data.to_sql('sua_tabela', conn, if_exists='replace', index=False)
+
+# Feche a conexão com o banco de dados
+conn.close()
+
+print("Banco de dados SQLite criado e dados carregados com sucesso!")
